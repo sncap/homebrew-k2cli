4c64cf579b4af5379616b03690ea5b53b6ff86ed
Alejandro Escobar <jaescobar.cell@gmail.com>
2017-05-31 07:32:59 -0700

    1.0.5 changed the depends_on for brewed and external install

--- k2cli.rb
+++ k2cli.rb
@@ -4,7 +4,7 @@ class K2cli < Formula
   url "https://github.com/samsung-cnct/k2cli/releases/download/1.0.5/k2cli_1.0.5_darwin_amd64.tar.gz"
   sha256 "7e5c0fcd1a696cdbdf0c9bab0035e1c6b336b3b8d81ad7d568fbe54743d67a55"
 
-  depends_on "docker" => :run
+  depends_on "docker"
 
   def install
     bin.install "k2cli"
