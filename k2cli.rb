8e8cfacc3eaa1bae24117be06b846d7bbf5dd16d
David Yun <sncap7@gmail.com>
2017-07-19 16:45:48 -0700

    Update k2cli.rb
    change the value of  sha256

--- k2cli.rb
+++ k2cli.rb
@@ -2,7 +2,7 @@ class K2cli < Formula
   desc "CLI for K2"
   homepage "https://github.com/samsung-cnct/k2cli"
   url "https://github.com/samsung-cnct/k2cli/releases/download/1.0.6/k2cli_1.0.6_darwin_amd64.tar.gz"
-  sha256 "0fb09d489b32414405dc128a452058ee14ec589f56637759eaf04451f58a5a46"
+  sha256 "f1a98fa2e77169fb9cd983c8f882ffc7429afafb84122a5b592eb29054ac0f9a"
 
   depends_on "docker"
 
